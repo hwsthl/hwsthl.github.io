69af1cb1bf1c4fe2a4cbe1008cc02ab663b60bf3
hwsthl <39776888+hwsthl@users.noreply.github.com>
2018-10-17 08:25:38 -0400

    lots of stuff

--- icanchange/index.js
+++ icanchange/index.js
@@ -192,3 +192,7 @@ window.onresize = function() {
 	function basicPopup(url) {
 popupWindow = window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 	}
+// Alert Function
+	function alertBox() {
+    alert("Please Allow Popups in Browser Settings and Turn on Audio Before Continuing");
+}
